51b323322984e38e6424a95fe28c33e18050f4cb
Ian lai <Leadmlg.wolfy@gmail.com>
2023-01-17 19:21:07 -0800

    Deleted unused code

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -100,8 +100,6 @@ public final class Constants {
     public static final class Ports {
       public static final int intakeLower = 15; // placeholder value
       public static final int intakeUpper = 9; // placeholder value
-      public static final int placeUpper = 8; // placeholder value
-      public static final int placeLower = 16; // placeholder value
     }
 
     public static final double intakePower = 0.25;
