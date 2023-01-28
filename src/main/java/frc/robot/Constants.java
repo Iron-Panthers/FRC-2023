c7682287cbe33b744725740a14a7a3040059a2ec
Isaac <isaac@rubenfamily.com>
2023-01-27 17:36:19 -0800

    trust cameras more

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -145,9 +145,9 @@ public final class Constants {
     public static final Matrix<N3, N1> VISION_MEASUREMENT_STANDARD_DEVIATIONS =
         Matrix.mat(Nat.N3(), Nat.N1())
             .fill(
-                2, // x
-                2, // y
-                2 * Math.PI // theta
+                .9, // x
+                .9, // y
+                .9 * Math.PI // theta
                 );
 
     public static final double CAMERA_CAPTURE_LATENCY_FUDGE_MS = 11;
