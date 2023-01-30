aa766c11d0b98fb4a7d53199c59348dddd2cafc5
Isaac <isaac@rubenfamily.com>
2023-01-30 00:53:32 -0800

    add a few more unit tests

--- src/test/java/frc/util/pathing/GraphPathfinderTests.java
+++ src/test/java/frc/util/pathing/GraphPathfinderTests.java
@@ -14,7 +14,7 @@ public class GraphPathfinderTests {
   }
 
   @UtilTest
-  public void pathfinderFindsPath() {
+  public void pathfinderFindsBasicPath() {
     Graph<Translation2d> graph = new Graph<>();
     graph.addEdge(coord(0, 0), coord(1, 0), 1);
     graph.addEdge(coord(1, 0), coord(2, 0), 1);
@@ -30,5 +30,51 @@ public class GraphPathfinderTests {
     assertEquals(
         Optional.of(List.of(coord(0, 0), coord(1, 0), coord(2, 0), coord(3, 0))),
         GraphPathfinder.findPath(graph, coord(0, 0), coord(3, 0)));
+  }
+
+  @UtilTest
+  public void pathfinderFindsCheaperEdgePath() {
+    Graph<Translation2d> graph = new Graph<>();
+    graph.addEdge(coord(0, 0), coord(1, 0), 1);
+    graph.addEdge(coord(1, 0), coord(2, 0), 1);
+    graph.addEdge(coord(2, 0), coord(3, 0), 1);
+    graph.addEdge(coord(3, 0), coord(4, 0), 1);
+    graph.addEdge(coord(4, 0), coord(5, 0), 1);
+
+    graph.addEdge(coord(0, 0), coord(0, 1), 4);
+    graph.addEdge(coord(0, 1), coord(0, 2), 4);
+
+    // teleport!
+    graph.addEdge(coord(5, 0), coord(0, 2), 2);
+
+    assertEquals(
+        Optional.of(
+            List.of(
+                coord(0, 0),
+                coord(1, 0),
+                coord(2, 0),
+                coord(3, 0),
+                coord(4, 0),
+                coord(5, 0),
+                coord(0, 2))),
+        GraphPathfinder.findPath(graph, coord(0, 0), coord(0, 2)));
+  }
+
+  @UtilTest
+  public void pathfinderReturnsEmpty() {
+    Graph<Translation2d> graph = new Graph<>();
+    graph.addEdge(coord(0, 0), coord(1, 0), 1);
+    graph.addEdge(coord(1, 0), coord(2, 0), 1);
+    graph.addEdge(coord(2, 0), coord(3, 0), 1);
+    graph.addEdge(coord(3, 0), coord(4, 0), 1);
+    graph.addEdge(coord(4, 0), coord(5, 0), 1);
+
+    graph.addEdge(coord(0, 0), coord(0, 1), 4);
+    graph.addEdge(coord(0, 1), coord(0, 2), 4);
+
+    // teleport!
+    graph.addEdge(coord(5, 0), coord(0, 2), 2);
+
+    assertEquals(Optional.empty(), GraphPathfinder.findPath(graph, coord(0, 0), coord(0, 3)));
   }
 }
