26867db4804424d7674448d4aa90b04f68df4031
Isaac <isaac@rubenfamily.com>
2023-01-30 01:30:16 -0800

    test closest point fn

--- src/test/java/frc/util/pathing/RubenManueverGeneratorTests.java
+++ src/test/java/frc/util/pathing/RubenManueverGeneratorTests.java
@@ -1,5 +1,7 @@
 package frc.util.pathing;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import au.com.origin.snapshots.Expect;
 import au.com.origin.snapshots.junit5.SnapshotExtension;
 import edu.wpi.first.math.geometry.Translation2d;
@@ -16,15 +18,35 @@ public class RubenManueverGeneratorTests {
     return new Translation2d(x, y);
   }
 
-  public static Stream<Arguments> rubenManueverFindFullPathMatchesSnapshotProvider() {
-    return Stream.of(Arguments.of(coord(1, 1), coord(7, 7)));
+  public static Stream<Arguments> getClosestPointProvider() {
+    return Stream.of(
+        Arguments.of(coord(2.001, 2), coord(2, 2)),
+        Arguments.of(coord(3.12, 2.01), coord(3.1, 2)),
+        Arguments.of(coord(3.12, 2.06), coord(3.1, 2.1)),
+        Arguments.of(coord(3.12, -.01), coord(3.1, 0))
+        // brace holder
+        );
+  }
+
+  @UtilParamTest
+  @MethodSource("getClosestPointProvider")
+  public void getClosestPoint(Translation2d point, Translation2d expected) {
+    assertEquals(expected, RubenManueverGenerator.getClosestPoint(point));
+  }
+
+  public static Stream<Arguments> findFullPathMatchesSnapshotProvider() {
+    return Stream.of(
+        Arguments.of(coord(1, 1), coord(7, 7))
+
+        // load bearing comment (hold the final brace)
+        );
   }
 
   private Expect expect;
 
   @UtilParamTest
-  @MethodSource("rubenManueverFindFullPathMatchesSnapshotProvider")
-  public void rubenManueverFindFullPathMatchesSnapshot(Translation2d start, Translation2d end) {
+  @MethodSource("findFullPathMatchesSnapshotProvider")
+  public void findFullPathMatchesSnapshot(Translation2d start, Translation2d end) {
     RubenManueverGenerator rubenManueverGenerator = new RubenManueverGenerator();
 
     var path =
