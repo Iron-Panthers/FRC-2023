ca3211dc050f71b4b3e3c4576b89c033189bf9fa
Isaac <isaac@rubenfamily.com>
2023-01-29 19:49:23 -0800

    test rect behaves right

--- src/test/java/frc/util/pathing/FieldObstructionMapTests.java
+++ src/test/java/frc/util/pathing/FieldObstructionMapTests.java
@@ -1,15 +1,41 @@
 package frc.util.pathing;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import au.com.origin.snapshots.Expect;
 import au.com.origin.snapshots.junit5.SnapshotExtension;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import edu.wpi.first.math.geometry.Translation2d;
 import frc.UtilTest;
+import frc.util.pathing.FieldObstructionMap.RectangleObstruction;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 @ExtendWith({SnapshotExtension.class})
 public class FieldObstructionMapTests {
+
+  @UtilTest
+  public void rectangleContainsWorks() {
+    RectangleObstruction rect =
+        new RectangleObstruction(
+            FieldObstructionMap.AllianceColor.RED,
+            "test",
+            new Translation2d(0, 0),
+            new Translation2d(1, 1));
+
+    assertTrue(
+        rect.contains(new Translation2d(0, 0)), "Point (0, 0) should be in rect (0, 0) -> (1, 1)");
+    assertTrue(
+        rect.contains(new Translation2d(1, 1)), "Point (1, 1) should be in rect (0, 0) -> (1, 1)");
+    assertTrue(
+        rect.contains(new Translation2d(0.5, 0.5)),
+        "Point (0.5, 0.5) should be in rect (0, 0) -> (1, 1)");
+
+    assertFalse(
+        rect.contains(new Translation2d(1.1, 1.1)),
+        "Point (1.1, 1.1) should not be in rect (0, 0) -> (1, 1)");
+  }
 
   @UtilTest
   public void obstructionMapMatchesSnapshot(Expect expect) {
