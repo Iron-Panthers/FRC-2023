5e312d67bdc871a175f35b249bd3d5b9ec2a7721
Isaac <isaac@rubenfamily.com>
2023-02-08 16:28:26 -0800

    flip video

--- linuxWifiStream.sh
+++ linuxWifiStream.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/bash
 
-gst-launch-1.0 -e udpsrc port=5801 ! "application/x-rtp, clock-rate=(int)90000, media=(string)video, encoding-name=VP8" ! rtpvp8depay ! tee name = t t. ! queue ! decodebin ! autovideosink 
+gst-launch-1.0 -e udpsrc port=5801 ! "application/x-rtp, clock-rate=(int)90000, media=(string)video, encoding-name=VP8" ! rtpvp8depay ! tee name = t t. ! queue ! decodebin ! videoflip method=rotate-180 ! autovideosink 
