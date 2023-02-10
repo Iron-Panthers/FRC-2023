615589362ed57bd0d5a51a233f2735106c1f1ae4
Brandon <25nguyenb26@stu.smuhsd.org>
2023-02-09 20:15:37 -0800

    Deadbanding

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -74,7 +74,8 @@ public class RobotContainer {
             () -> (-modifyAxis(will.getLeftX()) * Drive.MAX_VELOCITY_METERS_PER_SECOND),
             will.rightBumper()));
 
-    armSubsystem.setDefaultCommand(new AngleArmCommand(armSubsystem, jason::getRightY));
+    armSubsystem.setDefaultCommand(
+        new AngleArmCommand(armSubsystem, () -> ControllerUtil.deadband(jason.getLeftY(), 0.2)));
 
     SmartDashboard.putBoolean("is comp bot", MacUtil.IS_COMP_BOT);
 
