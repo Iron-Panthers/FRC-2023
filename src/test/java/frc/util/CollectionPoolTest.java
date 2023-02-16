a1d8198bc20163b85458380b343cc5b40e0e1a47
Isaac <isaac@rubenfamily.com>
2023-02-15 23:01:21 -0800

    finish testing collectionpool

--- src/test/java/frc/util/CollectionPoolTest.java
+++ src/test/java/frc/util/CollectionPoolTest.java
@@ -21,7 +21,6 @@ public class CollectionPoolTest {
     }
 
     public void reset() {
-      id = 0;
       resetCount++;
     }
   }
@@ -51,5 +50,51 @@ public class CollectionPoolTest {
     var collection2 = pool.get();
     assertEquals(2, collection2.id);
     assertEquals(0, collection2.resetCount);
+  }
+
+  @UtilTest
+  public void collectionPoolPuts() {
+    var supplier = createIncrementingSupplier();
+    CollectionPool<FakeCollection> pool = new CollectionPool<>(supplier, FakeCollection::reset);
+
+    var collection0 = pool.get();
+    var collection1 = pool.get();
+    var collection2 = pool.get();
+
+    pool.put(collection1);
+
+    var collection3 = pool.get();
+    assertEquals(1, collection3.id);
+    assertEquals(1, collection3.resetCount);
+
+    pool.put(collection3);
+
+    var collection4 = pool.get();
+    assertEquals(1, collection4.id);
+    assertEquals(2, collection4.resetCount);
+
+    assertEquals(collection1, collection4);
+  }
+
+  @UtilTest
+  public void collectionPoolOnlyConstructsIfEmpty() {
+    var supplier = createIncrementingSupplier();
+    CollectionPool<FakeCollection> pool = new CollectionPool<>(supplier, FakeCollection::reset);
+
+    var collection0 = pool.get();
+    var collection1 = pool.get();
+    var collection2 = pool.get();
+
+    pool.put(collection0);
+    pool.put(collection1);
+    pool.put(collection2);
+
+    var collection3 = pool.get();
+    var collection4 = pool.get();
+    var collection5 = pool.get();
+
+    assertEquals(collection0, collection5);
+    assertEquals(collection1, collection4);
+    assertEquals(collection2, collection3);
   }
 }
