f0c611b8e186d1dcedea08c41b818dea9e108f5f
Isaac <isaac@rubenfamily.com>
2023-02-28 20:29:51 -0800

    encode the intake positions

--- src/main/java/frc/util/NodeSelectorUtility.java
+++ src/main/java/frc/util/NodeSelectorUtility.java
@@ -1,6 +1,7 @@
 package frc.util;
 
-import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import java.util.List;
 
 public class NodeSelectorUtility {
@@ -19,18 +20,29 @@ public class NodeSelectorUtility {
   public static final int WIDTH = 9;
   public static final int HEIGHT = 3;
 
-  public static record NodeStack(int number, Translation2d position, NodeType type) {}
+  public static record NodeStack(int number, Pose2d position, NodeType type) {}
+
+  private static final double X_POSITION = 1.8;
 
   // one is on the substation, counting up to the other side
-  public static List<NodeStack> nodeStacks =
+  public static final List<NodeStack> nodeStacks =
       List.of(
-          new NodeStack(1, null, NodeType.CONE),
-          new NodeStack(2, null, NodeType.CUBE),
-          new NodeStack(3, null, NodeType.CONE),
-          new NodeStack(4, null, NodeType.CONE),
-          new NodeStack(5, null, NodeType.CUBE),
-          new NodeStack(6, null, NodeType.CONE),
-          new NodeStack(7, null, NodeType.CONE),
-          new NodeStack(8, null, NodeType.CUBE),
-          new NodeStack(9, null, NodeType.CONE));
+          new NodeStack(
+              1, new Pose2d(X_POSITION, 4.99, Rotation2d.fromDegrees(180)), NodeType.CONE),
+          new NodeStack(
+              2, new Pose2d(X_POSITION, 4.43, Rotation2d.fromDegrees(180)), NodeType.CUBE),
+          new NodeStack(
+              3, new Pose2d(X_POSITION, 3.87, Rotation2d.fromDegrees(180)), NodeType.CONE),
+          new NodeStack(
+              4, new Pose2d(X_POSITION, 3.31, Rotation2d.fromDegrees(180)), NodeType.CONE),
+          new NodeStack(
+              5, new Pose2d(X_POSITION, 2.76, Rotation2d.fromDegrees(180)), NodeType.CUBE),
+          new NodeStack(
+              6, new Pose2d(X_POSITION, 2.20, Rotation2d.fromDegrees(180)), NodeType.CONE),
+          new NodeStack(
+              7, new Pose2d(X_POSITION, 1.61, Rotation2d.fromDegrees(180)), NodeType.CONE),
+          new NodeStack(
+              8, new Pose2d(X_POSITION, 1.08, Rotation2d.fromDegrees(180)), NodeType.CUBE),
+          new NodeStack(
+              9, new Pose2d(X_POSITION, 0.25, Rotation2d.fromDegrees(180)), NodeType.CONE));
 }
