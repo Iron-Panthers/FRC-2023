890b47f5f5c2b9a468d130abf1926bf56479c66a
Isaac <isaac@rubenfamily.com>
2023-03-03 13:39:06 -0800

    refactor out duplicated stick logic

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -92,6 +92,12 @@ public class RobotContainer {
   /** the sendable chooser to select which auto to run. */
   private final SendableChooser<Command> autoSelector = new SendableChooser<>();
 
+  /* drive joystick "y" is passed to x because controller is inverted */
+  private final DoubleSupplier translationXSupplier =
+      () -> (-modifyAxis(will.getLeftY()) * Drive.MAX_VELOCITY_METERS_PER_SECOND);
+  private final DoubleSupplier translationYSupplier =
+      () -> (-modifyAxis(will.getLeftX()) * Drive.MAX_VELOCITY_METERS_PER_SECOND);
+
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Set up the default command for the drivetrain.
@@ -101,10 +107,7 @@ public class RobotContainer {
     // Right stick X axis -> rotation
     drivebaseSubsystem.setDefaultCommand(
         new DefaultDriveCommand(
-            drivebaseSubsystem,
-            () -> (-modifyAxis(will.getLeftY()) * Drive.MAX_VELOCITY_METERS_PER_SECOND),
-            () -> (-modifyAxis(will.getLeftX()) * Drive.MAX_VELOCITY_METERS_PER_SECOND),
-            will.rightBumper()));
+            drivebaseSubsystem, translationXSupplier, translationYSupplier, will.rightBumper()));
 
     armSubsystem.setDefaultCommand(
         new ArmManualCommand(
@@ -187,9 +190,8 @@ public class RobotContainer {
         .whileTrue(
             new RotateVelocityDriveCommand(
                 drivebaseSubsystem,
-                /* drive joystick "y" is passed to x because controller is inverted */
-                () -> (-modifyAxis(will.getLeftY()) * Drive.MAX_VELOCITY_METERS_PER_SECOND),
-                () -> (-modifyAxis(will.getLeftX()) * Drive.MAX_VELOCITY_METERS_PER_SECOND),
+                translationXSupplier,
+                translationYSupplier,
                 rotationVelocity,
                 will.rightBumper()));
 
@@ -200,8 +202,8 @@ public class RobotContainer {
         .onTrue(
             new RotateVectorDriveCommand(
                 drivebaseSubsystem,
-                () -> (-modifyAxis(will.getLeftY()) * Drive.MAX_VELOCITY_METERS_PER_SECOND),
-                () -> (-modifyAxis(will.getLeftX()) * Drive.MAX_VELOCITY_METERS_PER_SECOND),
+                translationXSupplier,
+                translationYSupplier,
                 will::getRightY,
                 will::getRightX,
                 will.rightBumper()));
