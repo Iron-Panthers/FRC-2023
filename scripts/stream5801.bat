e5db3392fef7cf7d0b383f22e45fb8b65e5f9381
Isaac <isaac@rubenfamily.com>
2023-03-04 12:01:41 -0800

    switch to bat

--- scripts/stream5801.bat
+++ scripts/stream5801.bat
@@ -1,3 +1,3 @@
+@echo off
 gst-launch-1.0 -e udpsrc port=5801 ! "application/x-rtp, clock-rate=(int)90000, media=(string)video, encoding-name=VP8" ! rtpvp8depay ! tee name = t t. ! queue ! decodebin ! videoflip method=rotate-180 ! autovideosink t. ! queue ! matroskamux ! filesink location=videos/%time:~0,2%_%time:~3,2%_%time:~6,2%__%date:~4,2%_%date:~7,2%_%date:~10,4%_5801.mkv
-
-Read-Host -Prompt "Press Enter to exit"
+pause
