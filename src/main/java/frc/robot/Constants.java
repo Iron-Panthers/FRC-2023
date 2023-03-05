4c929a1dbd76175f84616b436053002a3c227637
Isaac <isaac@rubenfamily.com>
2023-03-05 10:14:50 -0800

    bump stator current

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -356,7 +356,7 @@ public final class Constants {
           new OuttakeDetails(0.1, Optional.empty(), Optional.empty());
 
       public static final OuttakeDetails INTAKE =
-          new OuttakeDetails(1, Optional.of(new OuttakeDetails.StatorLimit(75)), Optional.of(2d));
+          new OuttakeDetails(1, Optional.of(new OuttakeDetails.StatorLimit(110)), Optional.of(2d));
 
       public static final OuttakeDetails OUTTAKE =
           new OuttakeDetails(-0.2, Optional.empty(), Optional.of(2d));
