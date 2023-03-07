ae5993abb7d4eb87d94fd18d4cb6b6cc921a21e8
Isaac <isaac@rubenfamily.com>
2023-03-06 18:29:15 -0800

    use the path

--- src/main/java/frc/robot/autonomous/commands/N2_1CubePlus1ConeEngage.java
+++ src/main/java/frc/robot/autonomous/commands/N2_1CubePlus1ConeEngage.java
@@ -6,20 +6,18 @@ package frc.robot.autonomous.commands;
 
 import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
+import com.pathplanner.lib.commands.FollowPathWithEvents;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants;
-import frc.robot.Constants.Arm;
-import frc.robot.commands.ArmPositionCommand;
 import frc.robot.commands.FollowTrajectoryCommand;
-import frc.robot.commands.ForceOuttakeSubsystemModeCommand;
 import frc.robot.commands.ScoreCommand;
-import frc.robot.commands.SetZeroModeCommand;
 import frc.robot.subsystems.ArmSubsystem;
 import frc.robot.subsystems.DrivebaseSubsystem;
 import frc.robot.subsystems.OuttakeSubsystem;
 import frc.util.NodeSelectorUtility.Height;
 import frc.util.NodeSelectorUtility.NodeType;
+import java.util.List;
 import java.util.Map;
 
 public class N2_1CubePlus1ConeEngage extends SequentialCommandGroup {
@@ -32,33 +30,23 @@ public class N2_1CubePlus1ConeEngage extends SequentialCommandGroup {
       ArmSubsystem armSubsystem,
       DrivebaseSubsystem drivebaseSubsystem) {
 
-    PathPlannerTrajectory path1 =
-        PathPlanner.loadPath(
+    List<PathPlannerTrajectory> paths =
+        PathPlanner.loadPathGroup(
             "n2 cone pickup to n3", maxVelocityMetersPerSecond, maxAccelerationMetersPerSecondSq);
 
     addCommands(
         // score the cube (later)
-        new FollowTrajectoryCommand(path1, true, drivebaseSubsystem)
-            .deadlineWith(
-                new SetZeroModeCommand(armSubsystem)
-                    .andThen(
-                        new ArmPositionCommand(armSubsystem, Arm.Setpoints.GROUND_INTAKE)
-                            .raceWith(
-                                new ForceOuttakeSubsystemModeCommand(
-                                        outtakeSubsystem, OuttakeSubsystem.Modes.INTAKE)
-                                    .withTimeout(4.5))
-                            .andThen(new ArmPositionCommand(armSubsystem, Arm.Setpoints.STOWED)))),
+        new FollowPathWithEvents(
+            new FollowTrajectoryCommand(paths.get(0), true, drivebaseSubsystem),
+            paths.get(0).getMarkers(),
+            eventMap),
         new ScoreCommand(
             outtakeSubsystem,
             armSubsystem,
-            Constants.SCORE_STEP_MAP.get(NodeType.CONE.atHeight(Height.HIGH)))
-
-        // new ScoreCommand(
-        //       outtakeSubsystem,
-        //       armSubsystem,
-        //       Constants.SCORE_STEP_MAP.get(scoreType),
-        //       jason.leftBumper())
-        // new BalanceCommand(drivebaseSubsystem)
-        );
+            Constants.SCORE_STEP_MAP.get(NodeType.CONE.atHeight(Height.HIGH))),
+        new FollowPathWithEvents(
+            new FollowTrajectoryCommand(paths.get(1), drivebaseSubsystem),
+            paths.get(1).getMarkers(),
+            eventMap));
   }
 }
