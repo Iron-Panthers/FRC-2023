50bc5f93a15c61437343f327b7599b6d89ca0271
Isaac <isaac@rubenfamily.com>
2023-03-07 18:37:56 -0800

    make autodrive a little easier

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -366,7 +366,7 @@ public final class Constants {
 
     public static final double RESPECT_CURRENT_VELOCITY_THRESHOLD_MS = .2;
 
-    public static final double ANTICIPATED_PATH_SOLVE_TIME_SECONDS = 1;
+    public static final double ANTICIPATED_PATH_SOLVE_TIME_SECONDS = .7;
 
     public static final class Costs {
       public static final int CARDINAL = 2;
