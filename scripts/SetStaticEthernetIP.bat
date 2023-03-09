c6a20deef8d4ebb15d71e598f3f0eeac5e0d8d9d
audrey <audreypjohnson@gmail.com>
2023-03-08 17:52:39 -0800

    (no branch): Auto stash before checking out "origin/feat/camera-scripts"

--- scripts/SetStaticEthernetIP.bat
+++ scripts/SetStaticEthernetIP.bat
@@ -3,8 +3,7 @@ echo off
 set "params=%*"
 cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
 
-netsh interface ip set address name="Wi-Fi" source=dhcp
+netsh interface ip set address name="Ethernet 5" source=static ^ address=10.50.26.5 mask=255.255.255.0 gate=10.10.0.1
 
-netsh interface ip set address name="Ethernet 5" source=dhcp
 
 exit
