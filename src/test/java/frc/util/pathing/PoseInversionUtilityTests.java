591d58ae518eaa063465183f98c8bf00c84d7200
Isaac <isaac@rubenfamily.com>
2023-03-10 17:48:18 -0800

    create pose inversion test

--- src/test/java/frc/util/pathing/PoseInversionUtilityTests.java
+++ src/test/java/frc/util/pathing/PoseInversionUtilityTests.java
@@ -2,10 +2,19 @@ package frc.util.pathing;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import au.com.origin.snapshots.Expect;
+import au.com.origin.snapshots.junit5.SnapshotExtension;
+import com.pathplanner.lib.PathConstraints;
+import com.pathplanner.lib.PathPlanner;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import frc.UtilTest;
+import frc.robot.Constants.Pathing;
+import frc.util.pathing.DisplayFieldArray.FieldSquare;
+import org.junit.jupiter.api.extension.ExtendWith;
 
+@ExtendWith({SnapshotExtension.class})
 public class PoseInversionUtilityTests {
   @UtilTest
   public void originPositionInverts() {
@@ -42,5 +51,55 @@ public class PoseInversionUtilityTests {
         new Pose2d(
             FieldObstructionMap.FIELD_LENGTH - 15.443, 7.410, Rotation2d.fromDegrees(180 + 0)),
         inverted);
+  }
+
+  private static FieldSquare[][] placeObstructions() {
+    FieldSquare[][] fieldSquares = new FieldSquare[Pathing.CELL_X_MAX][Pathing.CELL_Y_MAX];
+
+    for (int x = 0; x < Pathing.CELL_X_MAX; x++) {
+      for (int y = 0; y < Pathing.CELL_Y_MAX; y++) {
+        final double xCoord = x * Pathing.CELL_SIZE_METERS;
+        final double yCoord = y * Pathing.CELL_SIZE_METERS;
+        fieldSquares[x][y] =
+            FieldObstructionMap.isInsideObstruction(new Translation2d(xCoord, yCoord))
+                ? FieldSquare.OBSTRUCTION
+                : FieldSquare.EMPTY;
+      }
+    }
+
+    return fieldSquares;
+  }
+
+  private Expect expect;
+
+  @UtilTest
+  public void trajectoryStateInversionMatchesSnapshot() {
+    var fieldSquares = placeObstructions();
+    var path = PathPlanner.loadPath("n2 engage", new PathConstraints(4, 2));
+
+    var states = path.getStates();
+
+    for (var state : states) {
+      {
+        final var inverted = PoseInversionUtility.findRedState(state);
+        final var x =
+            (int) (inverted.poseMeters.getTranslation().getX() / Pathing.CELL_SIZE_METERS);
+        final var y =
+            (int) (inverted.poseMeters.getTranslation().getY() / Pathing.CELL_SIZE_METERS);
+        fieldSquares[x][y] = FieldSquare.PATH;
+      }
+
+      // also write the original pose to the field
+      {
+        final var x = (int) (state.poseMeters.getTranslation().getX() / Pathing.CELL_SIZE_METERS);
+        final var y = (int) (state.poseMeters.getTranslation().getY() / Pathing.CELL_SIZE_METERS);
+        fieldSquares[x][y] = FieldSquare.PATH;
+      }
+    }
+
+    StringBuilder sb = new StringBuilder();
+    DisplayFieldArray.renderField(sb, fieldSquares);
+
+    expect.toMatchSnapshot(sb);
   }
 }
