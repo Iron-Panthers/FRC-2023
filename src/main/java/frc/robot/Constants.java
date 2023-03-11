6d9ec32d048ef5cee335f3cab7ec7f8483413bad
Brandon <25nguyenb26@stu.smuhsd.org>
2023-03-10 22:30:49 -0800

    Tunings
    Jason helped tune the arm, but we need practice field time to tune these

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -240,7 +240,7 @@ public final class Constants {
                   OuttakeSubsystem.Modes.OUTTAKE)),
           NodeType.CONE.atHeight(Height.LOW),
           List.of(
-              new ScoreStep(new ArmState(55, 15)).canWaitHere(),
+              new ScoreStep(new ArmState(24, 5)).canWaitHere(),
               new ScoreStep(OuttakeSubsystem.Modes.OUTTAKE)),
           NodeType.CUBE.atHeight(Height.HIGH),
           List.of(
@@ -250,13 +250,13 @@ public final class Constants {
               new ScoreStep(new ArmState(95, Arm.Setpoints.Extensions.MIN_EXTENSION))),
           NodeType.CUBE.atHeight(Height.MID),
           List.of(
-              new ScoreStep(new ArmState(80, Arm.Setpoints.Extensions.MIN_EXTENSION)),
-              new ScoreStep(new ArmState(80, 12)).canWaitHere(),
+              new ScoreStep(new ArmState(70, Arm.Setpoints.Extensions.MIN_EXTENSION)),
+              new ScoreStep(new ArmState(70, 2)).canWaitHere(),
               new ScoreStep(OuttakeSubsystem.Modes.OUTTAKE),
-              new ScoreStep(new ArmState(80, Arm.Setpoints.Extensions.MIN_EXTENSION)))
+              new ScoreStep(new ArmState(70, Arm.Setpoints.Extensions.MIN_EXTENSION))),
           NodeType.CUBE.atHeight(Height.LOW),
           List.of(
-              new ScoreStep(new ArmState(55, 15)).canWaitHere(),
+              new ScoreStep(new ArmState(24, 5)).canWaitHere(),
               new ScoreStep(OuttakeSubsystem.Modes.OUTTAKE)));
 
   public static final class Vision {
