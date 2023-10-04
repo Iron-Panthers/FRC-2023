6213ef325fd706f4d346c4890901f0ead32c5052
audrey <audreypjohnson@gmail.com>
2023-10-03 17:03:41 -0700

    Putting angle snapping back the way it was but with more angles

--- src/main/java/frc/robot/commands/RotateVectorDriveCommand.java
+++ src/main/java/frc/robot/commands/RotateVectorDriveCommand.java
@@ -35,6 +35,8 @@ public class RotateVectorDriveCommand extends CommandBase {
 
   private double angle = 0;
 
+  private static final double[] angles = {0, 45, 90, 135, 180, 225, 270, 315};
+
   /** Creates a new DefaultDriveCommand. */
   public RotateVectorDriveCommand(
       DrivebaseSubsystem drivebaseSubsystem,
@@ -69,7 +71,7 @@ public class RotateVectorDriveCommand extends CommandBase {
     double rotY = rotationYSupplier.getAsDouble();
     boolean isRobotRelative = isRobotRelativeSupplier.getAsBoolean();
 
-    double targetAngle = Util.vectorToAngle(-rotX, -rotY);
+    double targetAngle = Util.angleSnap(Util.vectorToAngle(-rotX, -rotY), angles);
 
     // if stick magnitude is greater then rotate angle mag
     if (Util.vectorMagnitude(rotX, rotY) > Drive.ROTATE_VECTOR_MAGNITUDE) {
