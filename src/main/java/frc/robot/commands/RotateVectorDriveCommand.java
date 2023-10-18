c6da38199a01c25bff71f81228af971b1800b4f9
Jay <jaykang445@gmail.com>
2023-10-18 14:49:36 -0700

    anglesnap takes joystick input directly
    maybe fix maybe not - slightly different from prev. attempt

--- src/main/java/frc/robot/commands/RotateVectorDriveCommand.java
+++ src/main/java/frc/robot/commands/RotateVectorDriveCommand.java
@@ -71,11 +71,11 @@ public class RotateVectorDriveCommand extends CommandBase {
     double rotY = rotationYSupplier.getAsDouble();
     boolean isRobotRelative = isRobotRelativeSupplier.getAsBoolean();
 
-    double targetAngle = Util.angleSnap(Util.vectorToAngle(-rotX, -rotY), angles);
+    double targetAngle = Util.vectorToAngle(-rotX, -rotY);
 
     // if stick magnitude is greater then rotate angle mag
     if (Util.vectorMagnitude(rotX, rotY) > Drive.ROTATE_VECTOR_MAGNITUDE) {
-      angle = isRobotRelative ? Util.normalizeDegrees(targetAngle + initialAngle) : targetAngle;
+      angle = targetAngle;
     }
 
     drivebaseSubsystem.driveAngle(new Pair<>(x, y), angle);
